f894aff4b406933f2374f01cbffa6430d2111c69
Aryanraval2030 <aryanraval1432@gmail.com>
2026-05-30 18:41:45 +0530

    updated

--- back_end/models/User.js
+++ back_end/models/User.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new mongoose.Schema({
   // for register
   name: String,
+  email: { type: String, unique: true },
   password: String,
 
   // fore add custom habits
